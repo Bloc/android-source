0c9a37e9987c65ad5f3f5dfbdc463edb0a3e8149
Stan Idesis <stan.idesis@gmail.com>
2014-02-11 12:07:00 -0800

    Our first Java code, yay!

---
+++ milestone_00_welcome_to_android_development/02_development_environment/jdk/src/com/bloc/firstpackage/MyFirstJavaClass.java
@@ -0,0 +1,9 @@
+package com.bloc.firstpackage;
+
+public class MyFirstJavaClass extends Object {
+
+	public static void main(String [] args) {
+		System.out.print("Welcome to Bloc's Android apprenticeship! You're on your way!\n");
+	}
+
+}
