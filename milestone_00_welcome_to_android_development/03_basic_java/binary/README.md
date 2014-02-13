eb56e8134f854e2d727437836889749b2e6f100e
Stan Idesis <stan.idesis@gmail.com>
2014-02-13 14:50:13 -0800

    binary source

---
+++ milestone_00_welcome_to_android_development/03_basic_java/binary/README.md
@@ -0,0 +1,31 @@
+## Your First Java Class
+
+To get this show on the road, open your command prompt and **CD** into this README's directory (jdk).
+
+Run the following command:
+
+``` bash
+javac -d bin -sourcepath src src/com/bloc/firstpackage/MyFirstJavaClass.java
+```
+
+If you see an error, check to make sure you've typed in the command exactly and try again.
+
+After it returns successfully, **CD** into the bin directory:
+
+``` bash
+cd bin/
+```
+
+From within bin, run the following command:
+
+``` bash
+java com.bloc.firstpackage.MyFirstJavaClass
+```
+
+You should see the following outputted to your prompt:
+
+```
+Welcome to Bloc's Android apprenticeship! You're on your way!
+```
+
+Congratulations, you've ran some pretty gnarly code! Ready for more? Of course you are.
---
+++ milestone_00_welcome_to_android_development/03_basic_java/binary/src/com/bloc/binary/BinaryExercises.java
@@ -0,0 +1,155 @@
+package com.bloc.binary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BinaryExercises extends Object {
+
+	public static void main(String [] args) {
+		System.out.print("Excited for some Binary exercises? We sure are!\n\nType your answers in the command prompt and hit return to submity.\n\nEnter 'Q' if you'd like to quit.\n\nLet's get started!\n\n");
+
+		while (!test1()) {
+			// Wait
+		}
+
+		while (!test2()) {
+			// Wait
+		}
+
+		while (!test3()) {
+			// Wait
+		}
+
+		while (!test4()) {
+			// Wait
+		}
+
+		while (!test5()) {
+			// Wait
+		}
+
+		while (!test6()) {
+			// Wait
+		}
+
+		System.out.print("Congratulations! It's over!\nFor future reference, type \"152 in hex\" into Google and watch the magic happen.\n");
+	}
+
+	private static boolean test1() {
+		System.out.print("1. Which DECIMAL number does 10 (BINARY) represent? Answer: ");
+		int input = getInteger();
+		if (input == 2) {
+			System.out.print("Great!\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test1();
+	}
+
+	private static boolean test2() {
+		System.out.print("2. How do you represent the number 10 (DECIMAL) in BINARY? Answer: ");
+		int input = getInteger();
+		if (input == 1010) {
+			System.out.print("Rock on!\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test2();
+	}
+
+	private static boolean test3() {
+		System.out.print("3. How do you represent the number 10 (DECIMAL) in HEXADECIMAL? Answer: ");
+		String input = getHex();
+		if (input.equals("a")) {
+			System.out.print("You're killing it!\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test3();
+	}
+
+	private static boolean test4() {
+		System.out.print("4. Please convert 0x42E3 (HEXADECIMAL) into BINARY. Answer: ");
+		String input = getBinary();
+		if (input.equals("100 0010 1110 0011") || input.equals("100001011100011")) {
+			System.out.print("You're getting this.\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test4();
+	}
+
+	private static boolean test5() {
+		System.out.print("5. Please convert 1001110101011011 into HEXADECIMAL. Answer: ");
+		String input = getHex();
+		if (input.equals("9d5b")) {
+			System.out.print("Way to go!\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test5();
+	}
+
+	private static boolean test6() {
+		System.out.print("6. Please convert 353 (DECIMAL) into HEXADECIMAL.\nHINT: Convert to binary first, then to hex\nAnswer: ");
+		String input = getHex();
+		if (input.equals("161")) {
+			System.out.print("All done!\n\n");
+			return true;
+		}
+		System.out.print("Sorry, try again!\n\n");
+		return test6();
+	}
+
+	private static Integer getInteger() {
+		String input = getInput();
+		try {
+			return Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			// Needs to be a number
+			return getInteger();
+		}
+	}
+
+	private static String getHex() {
+		String input = getInput().toLowerCase();
+		int indexOfX = input.indexOf("x");
+		if (indexOfX > 0 || input.startsWith("x")) {
+			input = input.substring(indexOfX + 1, input.length());
+		}
+
+		int indexOfHash = input.indexOf("#");
+		if (indexOfHash > 0 || input.startsWith("#")) {
+			input = input.substring(indexOfHash + 1, input.length());
+		}
+		return input;
+	}
+
+	private static String getBinary() {
+		String input = getInput();
+		int indexOfFirst1 = input.indexOf("1");
+		if (indexOfFirst1 > 0) {
+			input = input.substring(indexOfFirst1, input.length());
+		}
+		return input;
+	}
+
+	private static String getInput() {
+		String input = null;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			while ((input = br.readLine()) == null) {
+				// Do nothing
+			}
+			input = input.trim();
+		} catch (IOException io){
+			io.printStackTrace();
+		} finally {
+			if (input != null && input.toLowerCase().equals("q")) {
+				System.exit(0);
+			}
+			return input;
+		}
+	}
+}
