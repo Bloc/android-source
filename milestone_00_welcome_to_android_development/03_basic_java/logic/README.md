91492377d672ca1c2d1d95beb46e16a38b32136f
Stan Idesis <stan.idesis@gmail.com>
2014-02-17 14:04:16 -0800

    Logic exercise

---
+++ milestone_00_welcome_to_android_development/03_basic_java/logic/README.md
@@ -0,0 +1,25 @@
+## Logic
+
+Open your command prompt and **CD** into this README's directory (logic). In the `src` path is a file named `Logic.java`. Open it and fill in the portions of the code where the comments have indicated to do so.
+
+When you've completed your work, test your code by compiling it with the following command:
+
+``` bash
+javac -d bin/ src/com/bloc/logic/Logic.java
+```
+
+Correct any and all errors that appear. If something goes wrong during compilation, it will be a result from the modifications you've made.
+
+After it returns successfully, **CD** into the bin directory:
+
+``` bash
+cd bin/
+```
+
+From within bin, run the following command:
+
+``` bash
+java com.bloc.logic.Logic
+```
+
+If you see the _right_ message, you've rocked it! Commit your changes and push them to Github.
---
+++ milestone_00_welcome_to_android_development/03_basic_java/logic/src/com/bloc/logic/Logic.java
@@ -0,0 +1,76 @@
+package com.bloc.logic;
+
+// import com.libs.logic.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Logic extends Object {
+
+	public static void main(String [] args) {
+		StringBuffer buffer = new StringBuffer();
+
+		/*
+		 * ASSIGNMENT:
+		 * Change the following logical operation such that the statement evaluates to 'true' 
+		 */
+		if (false && true) { // Change something here
+			buffer.append("r");
+		} else {
+			buffer.append("w");
+		}
+
+		/*
+		 * ASSIGNMENT:
+		 * Change a single character in the following 4 lines of code in order to evaluate
+		 * the conditional as 'true'
+		 */
+		int x = 5; // Change here
+		int y = 10; // Or here
+		int z = 30; // Or here
+		if (x == 6 || y == 11 || z == 83) { // Or even here, who knows
+			buffer.append("i");
+		} else {
+			buffer.append("r");
+		}
+
+		/*
+		 * ASSIGNMENT:
+		 * Add a single operator to the conditional statement in order to evaluate it as 'true'
+		 */
+		boolean aSincereFalsehood = false;
+		if (aSincereFalsehood) { // Add an operator to this line
+			buffer.append("g");
+		} else {
+			buffer.append("o");
+		}
+
+		/*
+		 * ASSIGNMENT:
+		 * Change the comparison below in order for the statement to evaluate as 'true'
+		 */
+		if (200 > 200) {
+			buffer.append("h");
+		} else {
+			buffer.append("n");
+		}
+
+		/*
+		 * ASSIGNMENT:
+		 * Alter the statements within parenthesis in order for all 3 statements to evaluate
+		 * as 'true'
+		 */
+		boolean lastOne = false;
+		if ((8 < 0) && !(15 == 15) && (lastOne == true)) { // Modify this line
+			buffer.append("t");
+		} else {
+			buffer.append("g");
+		}
+
+		System.out.print("If the following word looks appealing: " + buffer.toString() + ", then you've done it!\n");
+		if (!buffer.toString().equals("right")) {
+			System.out.print("Try again!\n");
+		}
+	}
+}
