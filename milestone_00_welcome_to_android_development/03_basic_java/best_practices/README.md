aaa5fe54bb761bea7981a2f1ae16aaa45c21da0c
Stan Idesis <stan.idesis@gmail.com>
2014-02-19 10:58:26 -0800

    Best practices added

---
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/README.md
@@ -0,0 +1,5 @@
+## Best Practices
+
+Open your command prompt and **CD** into this README's directory (best_practices). In the `src` path is a file named `WorsPractices.java`. Open it and correct all of the ugliness you find within.
+
+Commit your changes to Github when you're finished, have your mentor look it over.
---
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -0,0 +1,35 @@
+package com.bloc.bestpractices;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WorstPractices extends Object
+{
+
+	public static void main(String [] args) {
+	int MAGIC_NUMBER = WorstPractices.animals(false);
+	MAGIC_NUMBER *= 5;
+	if (MAGIC_NUMBER > 18) {
+	while(MAGIC_NUMBER > 0)
+	{
+	MAGIC_NUMBER--;
+	}
+	}
+	}
+
+// animals
+// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
+// yeswellheresthething: a seed which helps generate the magic number
+// returns: a magical number
+private static int animals(boolean yeswellheresthething)
+{
+		/*
+			Start off with one of these
+		*/
+	int anint = yeswellheresthething ? 34 : 21;
+	float sparklesfairy = .5f;
+	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
+	} return (int) sparklesfairy * aInt;
+}
+}
