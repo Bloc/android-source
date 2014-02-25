fcc89d1647231247696b42f662e9a34569f73228
Stan Idesis <stan.idesis@gmail.com>
2014-02-24 16:12:15 -0800

    Updated polymorphism

--- milestone_01_java/01_polymorphism/polymorphism/README.md
+++ milestone_01_java/01_polymorphism/polymorphism/README.md
@@ -1,11 +1,11 @@
 ## Polymorphism
 
-Open your command prompt and **CD** into this README's directory (polymorphism). 
+Open your command prompt and **CD** into this README's directory (polymorphism). Edit `Main.java` and re-do the work indicated using polymorphism.
 
 After you've finished, compile your code by running the following command:
 
 ``` bash
-javac -d bin src/com/bloc/statics/*.java src/com/bloc/statics/appliances/*.java
+javac -d bin src/com/bloc/polymorph/*.java src/com/bloc/polymorph/pets/*.java
 ```
 
 After it compiles successfully, **CD** into the bin directory:
@@ -17,9 +17,9 @@ cd bin/
 From within bin, execute your code by running the following command:
 
 ``` bash
-java com.bloc.statics.Main
+java com.bloc.polymorph.Main
 ```
 
-If you see a nice congrats message, everything's been properly static-o-fied!
+If you see a nice congrats message, you've successfully poly-morphed!
 
 Commit and push your changes often.
--- milestone_01_java/01_polymorphism/polymorphism/src/com/bloc/polymorph/Main.java
+++ milestone_01_java/01_polymorphism/polymorphism/src/com/bloc/polymorph/Main.java
@@ -12,6 +12,8 @@ public class Main extends Object {
 		Snake snake = new Snake();
 		Tarantula tarantula = new Tarantula();
 
+		// Accomplish the below using polymorphism
+
 		dog.feed();
 		dog.wash();
 		dog.exercise();
@@ -31,6 +33,33 @@ public class Main extends Object {
 		tarantula.feed();
 		tarantula.wash();
 		tarantula.exercise();
+
+		// Accomplish the above using polymorphism
+
+		if (!(dog.isFed() && dog.isWashed() && dog.isExercised())) {
+			System.out.println("Your dog needs a little more attention");
+			System.exit(1);
+		}
+
+		if (!(cat.isFed() && cat.isWashed() && cat.isExercised())) {
+			System.out.println("Your cat needs a little more attention");
+			System.exit(1);
+		}
+
+		if (!(bird.isFed() && bird.isWashed() && bird.isExercised())) {
+			System.out.println("Your bird needs a little more attention");
+			System.exit(1);
+		}
+
+		if (!(snake.isFed() && snake.isWashed() && snake.isExercised())) {
+			System.out.println("Your snake needs a little more attention");
+			System.exit(1);
+		}
+
+		if (!(tarantula.isFed() && tarantula.isWashed() && tarantula.isExercised())) {
+			System.out.println("Your tarantula needs a little more attention");
+			System.exit(1);
+		}
 		
 		System.out.println("/************************/");
 		System.out.println("/*                      */");
