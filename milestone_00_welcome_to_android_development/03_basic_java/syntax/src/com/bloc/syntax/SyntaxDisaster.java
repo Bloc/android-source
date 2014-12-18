a5cb7c41263857e0c8d4e57fb0ad2036a9da2df4
Vinay Bhardwaj <vnaybrdwj@gmail.com>
2014-12-17 16:21:26 -0800

    Checkpoint 12 completed

--- milestone_00_welcome_to_android_development/03_basic_java/syntax/src/com/bloc/syntax/SyntaxDisaster.java
+++ milestone_00_welcome_to_android_development/03_basic_java/syntax/src/com/bloc/syntax/SyntaxDisaster.java
@@ -4,13 +4,13 @@ public class SyntaxDisaster extends Object {
 
 	// FIX THE CODE BELOW
 
-	public static void main(String [] args) {;}
-		if (true); {
-			int x = 5
+	public static void main(String [] args) {
+		if (true){
+			int x = 5;
 		}
-		int syntaxIsFun; 5;
-		/* The following line prints stuff out, I promise
-		System.out.println("If you can see this message, you've won!);
+		int syntaxIsFun=5;
+		// The following line prints stuff out, I promise
+		System.out.println("If you can see this message, you've won!");
 	}
 
 	// FIX THE CODE ABOVE
