5df116ec149d0a873453019e502ab3e4849a9060
Renee CS <voiceofrae@gmail.com>
2015-02-03 22:38:49 -0800

    Checkpoint 25 complete

--- milestone_01_java/01_polymorphism/polymorphism/src/com/bloc/polymorph/Main.java
+++ milestone_01_java/01_polymorphism/polymorphism/src/com/bloc/polymorph/Main.java
@@ -6,33 +6,41 @@ public class Main extends Object {
 
 	public static void main(String [] args) {
 
-		Dog dog = new Dog();
-		Cat cat = new Cat();
-		Bird bird = new Bird();
-		Snake snake = new Snake();
-		Tarantula tarantula = new Tarantula();
+		Pet dog = new Dog();
+		Pet cat = new Cat();
+		Pet bird = new Bird();
+		Pet snake = new Snake();
+		Pet tarantula = new Tarantula();
+
+Pet[] pets = {dog, cat, bird, snake, tarantula}; // array of pets
+
+for (int i = 0; i < pets.length; i++) {
+	pets[i].feed();
+	pets[i].wash();
+	pets[i].exercise();
+} // iterates through array and accomplishes all the actions below
 
 		// Accomplish the below using polymorphism
 
-		dog.feed();
-		dog.wash();
-		dog.exercise();
+		// dog.feed();
+		// dog.wash();
+		// dog.exercise();
 
-		cat.feed();
-		cat.wash();
-		cat.exercise();
+		// cat.feed();
+		// cat.wash();
+		// cat.exercise();
 
-		bird.feed();
-		bird.wash();
-		bird.exercise();
+		// bird.feed();
+		// bird.wash();
+		// bird.exercise();
 
-		snake.feed();
-		snake.wash();
-		snake.exercise();
+		// snake.feed();
+		// snake.wash();
+		// snake.exercise();
 
-		tarantula.feed();
-		tarantula.wash();
-		tarantula.exercise();
+		// tarantula.feed();
+		// tarantula.wash();
+		// tarantula.exercise();
 
 		// Accomplish the above using polymorphism
 
@@ -41,7 +49,7 @@ public class Main extends Object {
 		assert bird.isFed() && bird.isWashed() && bird.isExercised() : "Your bird needs a little more attention";
 		assert snake.isFed() && snake.isWashed() && snake.isExercised() : "Your snake needs a little more attention";
 		assert tarantula.isFed() && tarantula.isWashed() && tarantula.isExercised() : "Your tarantula needs a little more attention";
-		
+
 		System.out.println("/************************/");
 		System.out.println("/*                      */");
 		System.out.println("/*                      */");
