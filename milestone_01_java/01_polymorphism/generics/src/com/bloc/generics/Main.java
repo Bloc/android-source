4a47ba8d337c406c62c6b1f9c7f6275f6eab34d7
Renee CS <voiceofrae@gmail.com>
2015-02-05 14:39:37 -0800

    Checkpoint 26 complete

--- milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
+++ milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
@@ -11,6 +11,16 @@ public class Main extends Object {
 		 * Put a bunch of Toys in toyBox!
 		 */
 
+Thing pandorasBox = new Thing("Pandoras Box");
+Thing batGirl = new ActionFigure();
+Thing whyASpoon = new Spoon();
+Thing whereTheSidewalkEnds = new Book();
+
+toyBox.addToy(new Toy<Thing>(pandorasBox));
+toyBox.addToy(new Toy<Thing>(batGirl)); // why is it that I don't use ActionFigure here?
+toyBox.addToy(new Toy<Thing>(whyASpoon));
+toyBox.addToy(new Toy<Thing>(whereTheSidewalkEnds));
+
 		assert toyBox.getToyCount() > 0 : "Let's get some toys in that box!";
 		System.out.println("Inside your toybox you've got:");
 		for (int i = 0; i < toyBox.getToyCount(); i++) {
