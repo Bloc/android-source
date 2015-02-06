ec780ba3298e9440a85cd9cf7817dcd431c6263c
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-02-05 19:55:21 -0800

    Checkpoint 14 completed

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -11,30 +11,29 @@ public class WorstPractices extends Object {
 	 *	Fix code and comments below this block
 	/************************************************/
 
-	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+public static void main(String [] args) {
+	
+	int magicNumber = WorstPractices.animals(false);
+	magicNumber = 5;
+	if (magicNumber > 18) {
+		while(magicNumber > 0) {
+		magicNumber = 10;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
+/**
+ * This method calculates a magic number and returns it.
+ * numberSeed: a seed which helps generate the magic number
+ */
+private static int animals(boolean numberSeed)
 {
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
+	int thing = 21;
+	float floatThing = .5f;
+	for (int brown = 0; brown < thing; brown++) { 
+		floatThing = thing;
+	} 
+	return floatThing * thing;
 }
 
 	/************************************************
