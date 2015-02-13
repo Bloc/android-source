702af760037cac4e58c76c10c837a4a5a2fdfea8
Renee CS <voiceofrae@gmail.com>
2015-02-13 01:08:39 -0800

    Almost complete, just need to get it to actually send messages.

--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
@@ -1,10 +1,40 @@
 package com.bloc.singletons;
 
+import java.util.HashSet;
+
+import com.bloc.singletons.talkers.*;
+import com.bloc.singletons.listeners.*;
+import com.bloc.singletons.Speakerphone;
+import com.bloc.singletons.Talker;
+import com.bloc.singletons.Listener;
+
 public class Main extends Object {
 
+Speakerphone _speakerphone = Speakerphone._speakerphone;
+
+public static HashSet<Listener> audience = new HashSet<Listener>();
+public static HashSet<Listener> household = new HashSet<Listener>();
+
 	public static void main(String [] args) {
-		// Instantiate some talkers and some listeners
-		// Register listeners
-		// Send messages!
+
+    // Instantiate some talkers and some listeners
+    Singer whitneyHouston = new Singer();
+    Parent mom = new Parent();
+    AudienceMember fanGirl = new AudienceMember();
+    Pet spot = new Pet();
+
+    // Register listeners
+    audience.add(fanGirl);
+    household.add(spot);
+
+    // Send messages! - INCOMPLETE
+    whitneyHouston.getMessage();
+    mom.getMessage();
+
+    fanGirl.onMessageReceived();
+
+    // audience.shoutMessage(Singer(whitneyHouston));
+
 	}
+
 }
--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
@@ -1,5 +1,8 @@
 package com.bloc.singletons;
 
+import java.util.Iterator;
+import java.util.HashSet;
+
 /*
  * This is a singleton class which maintains communication
  * between classes and Listeners
@@ -10,11 +13,27 @@ public class Speakerphone extends Object {
 	 * @return the singleton instance of Speakerphone
 	 */
 
+// a set of listeners, stored as an HashSet
+public static HashSet<Listener> listenerList;
+
+public static Speakerphone _speakerphone; // contains the global instance: adding _ is general practice for signaling a singleton
+
+	public Speakerphone get() {
+		if (_speakerphone == null) {
+			_speakerphone = new Speakerphone();
+		}
+		return _speakerphone;
+	}
+
 	/*
 	 * addListener
 	 * Add a listener to Speakerphone's list
 	 * @param listener an instance of the Listener interface
 	 */
+
+public void addListener (Listener listener) {
+	listenerList.add(listener);
+}
 
 	/*
 	 * removeListener
@@ -22,11 +41,24 @@ public class Speakerphone extends Object {
 	 * @param listener the Listener to remove
 	 */
 
+public void removeListener (Listener listener) {
+	listenerList.remove(listener);
+}
+
 	/*
 	 * shoutMessage
 	 * Sends the message to all of the Listeners tracked by Speakerphone
 	 * @param talker a Talker whose message will be sent
 	 */
+
+public void shoutMessage(Talker talker) {
+	Iterator<Listener> shoutIterator = listenerList.iterator();
+	while (shoutIterator.hasNext()) {
+		shoutIterator.next().onMessageReceived("a message");
+	} // ends while
+} // ends shoutMessage
+
+// loop through the system and call the main method
 
 	/*
 	 * shoutMessage
@@ -40,9 +72,24 @@ public class Speakerphone extends Object {
 	 *		 http://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class)
 	 */
 
+public void shoutMessageTo(Talker talker, Class<?> cls) {
+	Iterator<Listener> shoutIterator = listenerList.iterator();
+	while (shoutIterator.hasNext()) {
+		if (cls.isAssignableFrom(cls)) {
+			shoutIterator.next().onMessageReceived("a message");
+		} // ends if
+	} // ends while
+} // ends shoutMessageTo
+
+// same as shout msg, but in loop if it's a class type, then shout message - otherwise, ignore it (class type is cls)
+
 	/*
 	 * removeAll
 	 * Removes all Listeners from Speakerphone
 	 */
+
+public void removeAll (Listener listener) {
+	listenerList.clear();
+}
 
 }
