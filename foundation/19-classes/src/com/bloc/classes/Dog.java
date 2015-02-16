c655f4533c7961248451e6e9ecd303e111c88a08
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-02-16 10:10:51 -0800

    checkpoint 19 added

--- foundation/19-classes/src/com/bloc/classes/Dog.java
+++ foundation/19-classes/src/com/bloc/classes/Dog.java
@@ -1,221 +1,281 @@
-package com.bloc.classes;
+
+ 
 
 class Dog {
     // The length of hair which
     final float HAIR_CUT_LENGTH = 0.15f;
     // Minimum weight that any Dog can be
     final float MIN_WEIGHT = 1.25f;
-	// Amount of weight to gain after eating
-	final float WEIGHT_GAIN = 0.25f;
-	// Amount of weight to lose after playing
-	final float WEIGHT_LOSS = 0.2f;
-	// Hair length
-	float mHairLength;
-	// Gender, either "male" or "female"
-	String mGender;
-	// Size, either "tiny", "small", "average", or "large"
-	String mSize;
-	// Its age
-	int mAge;
-	// Its weight in pounds
-	float mWeight;
-	// The color of its coat
-	String mColor;
+    // Amount of weight to gain after eating
+    final float WEIGHT_GAIN = 0.25f;
+    // Amount of weight to lose after playing
+    final float WEIGHT_LOSS = 0.2f;
+    // Hair length
+    float mHairLength;
+    // Gender, either "male" or "female"
+    String mGender[] = {"male", "female"};
+    // Size, either "tiny", "small", "average", or "large"
+    String[] mSize = {"tiny", "small", "average", "large"};
+    // Its age
+    int mAge;
+    // Its weight in pounds
+    float mWeight;
+    // The color of its coat
+    String mColor;
 
-	/************************************************
-	 * ADD MEMBER VARIABLES HERE IF NECESSARY	
-	/************************************************/
+    /************************************************
+     * ADD MEMBER VARIABLES HERE IF NECESSARY      
+    /************************************************/
+    int dogGender;
+    int dogSize;
+    int meals;
+    int plays;
+    
+
+    /*
+     * getHairLength
+     *
+     * @return this Dog's hair length (float)
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getHairLength method
+    /************************************************/
+float getHairLength(){
+    return mHairLength;
+}
+    /*
+     * setHairLength
+     *
+     * Sets the length of the Dog's hair (float)
+     *
+     * @param hairLength the new length of the hair (float)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setHairLength method
+    /************************************************/
+void setHairLength(float hairLength){
+    mHairLength = hairLength;
+}
+    /*
+     * getGender
+     *
+     * @return this Dog's gender (String)
+     */
+    // ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getGender method
+    /************************************************/
+String getGender(){
+    return mGender[dogGender];
+}
+    /*
+     * setGender
+     *
+     * Sets this Dog's gender
+     *
+     * @param gender the new gender of the Dog (String)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setGender method
+    /************************************************/
+void setGender(String gender){
+    mGender[dogGender] = gender;
+}
+    /*
+     * getSize
+     *
+     * @return the size of the dog (String)
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getSize method
+    /************************************************/
+String getSize(){
+    return mSize[dogSize];
+}
+    /*
+     * setSize
+     *
+     * Sets the size of the Dog
+     *
+     * @param size the new size of the Dog (String)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setSize method
+    /************************************************/
+void setSize(String size){
+    mSize[dogSize] = size;
+}
+    /*
+     * getAge
+     *
+     * @return this Dog's age (int)
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getAge method
+    /************************************************/
+int getAge(){
+    return mAge;
+}
+    /*
+     * setAge
+     *
+     * Sets the age of the Dog
+     *
+     * @param age the new age of the Dog (int)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setAge method
+    /************************************************/
+void setAge(int age){
+    mAge = age;
+}
+    /*
+     * getWeight
+     *
+     * @return this Dog's weight (float)
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getWeight method
+    /************************************************/
+float getWeight(){
+    return mWeight;
+}
+    /*
+     * setWeight
+     *
+     * Sets the weight of the Dog
+     *
+     * @param weight the new weight of the Dog (float)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setWeight method
+    /************************************************/
+void setWeight(float weight){
+    mWeight = weight;
+}
+    /*
+     * getColor
+     *
+     * @return this Dog's color (String)
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the getColor method
+    /************************************************/
+String getColor(){
+    return mColor;
+}
+    /*
+     * setColor
+     *
+     * Sets the color of the Dog
+     *
+     * @param color the new color of the Dog's coat (String)
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the setColor method
+    /************************************************/
+void setColor(String color){
+    mColor = color;
+}
+    /*
+     * feed
+     *
+     *
+     * Feeds the Dog.
+     *
+     * Side-effect: 1. The Dog gains weight, specifically WEIGHT_GAIN
+     *              2. Every 3 meals, the Dog grows to a larger size, if
+     *                 possible
+     *                 i.e. "tiny" (3 meals later ->) "small" (3 meals later ->)
+     *                 "average" (3 meals later ->) "large"
+     *                 the Dog cannot exceed the "large" size or shrink smaller than
+     *                 "tiny"
+     * @return nothing
+     */ 
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the feed method
+    /************************************************/
+void feed(){
+    mWeight += WEIGHT_GAIN;
+    if (dogSize == 0)
+    setSize(mSize[1]);
+    //it works this way, but after 4 invocations it goes straight to large
+    if(++meals==2 &&getSize()!= mSize[mSize.length-1]){ 
+    dogSize++;
+    getSize();
+    meals = 1;   
+    }
+   
+}
 
 
-	/*
-	 * getHairLength
-	 *
-	 * @return this Dog's hair length (float)
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getHairLength method
-	/************************************************/
+    /*
+     * play
+     *
+     * Let the Dog play.
+     *
+     * Side-effect: 1. The Dog loses weight, specifically WEIGHT_LOSS
+     *              2. Every 6 play invocations, the Dog shrinks to a smaller 
+     *                 size, if possible i.e. "large" (6 plays later->) "average" (6 plays later->) 
+     *                 "small" -> "tiny"
+     *              3. The Dog cannot shrink to a weight smaller than
+     *                 MIN_WEIGHT
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the play method
+    /************************************************/
+void play (){
+    mWeight -= WEIGHT_LOSS;
+    plays ++;
+    if(dogSize ==0)
+    setSize(mSize[mSize.length-1]);
+    if (mWeight < MIN_WEIGHT)
+    setWeight(MIN_WEIGHT);
+    if (plays%6==0 && dogSize <= mSize.length-1 && dogSize >0){
+    dogSize --;  
+    getSize();
+    plays = 1;
+    }
 
-	/*
-	 * setHairLength
-	 *
-	 * Sets the length of the Dog's hair (float)
-	 *
-	 * @param hairLength the new length of the hair (float)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setHairLength method
-	/************************************************/
-
-	/*
-	 * getGender
-	 *
-	 * @return this Dog's gender (String)
-	 */
-	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getGender method
-	/************************************************/
-
-	/*
-	 * setGender
-	 *
-	 * Sets this Dog's gender
-	 *
-	 * @param gender the new gender of the Dog (String)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setGender method
-	/************************************************/
-
-	/*
-	 * getSize
-	 *
-	 * @return the size of the dog (String)
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getSize method
-	/************************************************/
-
-	/*
-	 * setSize
-	 *
-	 * Sets the size of the Dog
-	 *
-	 * @param size the new size of the Dog (String)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setSize method
-	/************************************************/
-
-	/*
-	 * getAge
-	 *
-	 * @return this Dog's age (int)
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getAge method
-	/************************************************/
-
-	/*
-	 * setAge
-	 *
-	 * Sets the age of the Dog
-	 *
-	 * @param age the new age of the Dog (int)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setAge method
-	/************************************************/
-
-	/*
-	 * getWeight
-	 *
-	 * @return this Dog's weight (float)
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getWeight method
-	/************************************************/
-
-	/*
-	 * setWeight
-	 *
-	 * Sets the weight of the Dog
-	 *
-	 * @param weight the new weight of the Dog (float)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setWeight method
-	/************************************************/
-
-	/*
-	 * getColor
-	 *
-	 * @return this Dog's color (String)
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the getColor method
-	/************************************************/
-
-	/*
-	 * setColor
-	 *
-	 * Sets the color of the Dog
-	 *
-	 * @param color the new color of the Dog's coat (String)
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the setColor method
-	/************************************************/
-
-	/*
-	 * feed
-	 *
-	 * Feeds the Dog.
-	 *
-	 * Side-effect: 1. The Dog gains weight, specifically WEIGHT_GAIN
-	 *              2. Every 3 meals, the Dog grows to a larger size, if
-	 *                 possible
-	 *                 i.e. "tiny" (3 meals later ->) "small" (3 meals later ->)
-	 *                 "average" (3 meals later ->) "large"
-	 *				   the Dog cannot exceed the "large" size or shrink smaller than
-	 *				   "tiny"
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the feed method
-	/************************************************/
-
-	/*
-	 * play
-	 *
-	 * Let the Dog play.
-	 *
-	 * Side-effect: 1. The Dog loses weight, specifically WEIGHT_LOSS
-	 *				2. Every 6 play invocations, the Dog shrinks to a smaller 
-	 *		   		   size, if possible i.e. "large" (6 plays later->) "average" (6 plays later->) 
-	 *		   		   "small" -> "tiny"
-	 *		   		3. The Dog cannot shrink to a weight smaller than
-	 *		  		   MIN_WEIGHT
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the play method
-	/************************************************/
-
-	/*
-	 * cutHair
-	 *
-	 * Cut the Dog's hair.
-	 *
-	 * Side-effect: the Dog's hair length is reduced by HAIR_CUT_LENGTH
-	 * 				The Dog's hair cannot be shorter than 0f
-	 *
-	 * @return nothing
-	 */
-	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Create the cutHair method
-	/************************************************/
-
+}
+    /*
+     * cutHair
+     *
+     * Cut the Dog's hair.
+     *
+     * Side-effect: the Dog's hair length is reduced by HAIR_CUT_LENGTH
+     *              The Dog's hair cannot be shorter than 0f
+     *
+     * @return nothing
+     */
+    /************************************************
+     *  ASSIGNMENT:
+     *  Create the cutHair method
+    /************************************************/
+void cutHair(){
+    mHairLength -= HAIR_CUT_LENGTH;
+    if (getHairLength() < 0f)
+    setHairLength(0f);
+}
 }
