dafdba5e8884daf707fc89a630c8897289ae5bc0
Renee CS <voiceofrae@gmail.com>
2015-02-15 19:01:39 -0800

    Checkpoint complete? It passes the tests, pinned for review w/John

--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
@@ -1,8 +1,45 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
 	@Override
 	public void run() {
 		// TODO
+
+    try {
+      File existingImage = new File("google_logo.png");
+      if (existingImage.exists()) {
+        existingImage.delete();
+      }
+      URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+      BufferedImage bufferedImage = ImageIO.read(url);
+      File outputfile = new File("google_logo.png");
+      ImageIO.write(bufferedImage, "png", outputfile);
+      if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+        Runtime.getRuntime().exec("open google_logo.png");
+      } else {
+        Runtime.getRuntime().exec("google_logo.png");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    File logo = new File("google_logo.png");
+    boolean exists = false;
+    try {
+      exists = logo.exists();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
 	}
 }
--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
@@ -11,40 +11,18 @@ public class Main extends Object {
 
 		// Extract content beneath to ImageGetter
 
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		File logo = new File("google_logo.png");
-		boolean exists = false;
-		try {
-			exists = logo.exists();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+    File logo = new File("google_logo.png");
+    boolean exists = false;
+    try {
+      exists = logo.exists();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
 
 		// ^^^ Extract the above to ImageGetter
 
-		// This shouldn't exist yet, therefore we should be able to print 
+		// This shouldn't exist yet, therefore we should be able to print
 		if (exists == false) {
 			System.out.println("/************************/");
 			System.out.println("/*                      */");
@@ -53,7 +31,7 @@ public class Main extends Object {
 			System.out.println("/*   congratulations!   */");
 			System.out.println("/*                      */");
 			System.out.println("/*                      */");
-			System.out.println("/************************/");	
+			System.out.println("/************************/");
 		}
 	}
 }
