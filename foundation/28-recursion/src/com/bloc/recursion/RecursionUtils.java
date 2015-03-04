4f5de743d81333ee8565bbffd25200d22eeb1113
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-03-03 18:50:40 -0800

    Checkpoint 28 completed

--- foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
+++ foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -3,23 +3,36 @@ package com.bloc.recursion;
 import java.util.*;
 
 public class RecursionUtils extends Object {
-	/*
-	 * findMaxRecursively
-	 *
-	 * Takes a list of numbers and finds the largest among them
-	 * using recursive calls.
-	 *
-	 * @param numbers a list of numbers, can be odd or even numbered
-	 * @return the largest number in the list
-	 *
-	 * Hint: your base case may be a comparison of 2 numbers
-	 */
-	public static final int findMaxRecursively(List<Integer> numbers) {
-		/************************************************
-	 	 *	ASSIGNMENT:
-	 	 *	Implement this method, the return value must
-	 	 *	change
-		/************************************************/
-		return 0;
-	}
+    /*
+     * findMaxRecursively
+     *
+     * Takes a list of numbers and finds the largest among them
+     * using recursive calls.
+     *
+     * @param numbers a list of numbers, can be odd or even numbered
+     * @return the largest number in the list
+     *
+     * Hint: your base case may be a comparison of 2 numbers
+     */
+    public static final int findMaxRecursively(List<Integer> numbers) {
+        /************************************************
+         *  ASSIGNMENT:
+         *  Implement this method, the return value must
+         *  change
+        /************************************************/
+
+        if(numbers.size() == 1)
+         return numbers.get(0);
+     
+    List<Integer> bottomHalf = numbers.subList(0,numbers.size()/2);
+     int bottom = findMaxRecursively(bottomHalf);
+
+     List<Integer> topHalf = numbers.subList(numbers.size()/2,numbers.size());
+     int top = findMaxRecursively(topHalf);
+
+     return top>bottom?top:bottom;
+
+         }
+        
 }
+
