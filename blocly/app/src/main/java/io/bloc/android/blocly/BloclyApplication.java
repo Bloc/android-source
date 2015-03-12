e503e82de855c980180f8208f6d2aaea9fa07d6a
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-03-11 23:58:30 -0700

    checkpoint 35 completed

---
+++ blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -0,0 +1,35 @@
+package io.bloc.android.blocly;
+
+import android.app.Application;
+
+/**
+ * Created by tonyk_000 on 3/11/2015.
+ */
+
+    public class BloclyApplication extends Application {
+
+        // #1
+        public static BloclyApplication getSharedInstance() {
+            return sharedInstance;
+        }
+
+        // #2
+        public static DataSource getSharedDataSource() {
+            return BloclyApplication.getSharedInstance().getDataSource();
+        }
+
+        private static BloclyApplication sharedInstance;
+        private DataSource dataSource;
+
+        // #3
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            sharedInstance = this;
+            dataSource = new DataSource();
+        }
+
+        public DataSource getDataSource() {
+            return dataSource;
+        }
+}
