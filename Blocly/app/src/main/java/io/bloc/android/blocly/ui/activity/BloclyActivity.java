a111ae7c78c15326a6cfd2e17064902f9c2b8c98
Renee CS <voiceofrae@gmail.com>
2015-03-17 00:43:05 -0700

    initial commit 2.0

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,17 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by ReneeCS on 3/17/15.
+ */
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
