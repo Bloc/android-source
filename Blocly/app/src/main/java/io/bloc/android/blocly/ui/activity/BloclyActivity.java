6fa3359325c33045302ffd7d7c6fd63ef302aece
Renee CS <voiceofrae@gmail.com>
2015-03-17 21:59:33 -0700

    Checkpoint 35 assignment complete :D

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,6 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
@@ -11,6 +12,9 @@ import io.bloc.android.blocly.R;
  * Created by ReneeCS on 3/17/15.
  */
 public class BloclyActivity extends Activity {
+
+    public TextView mAppTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,7 @@ public class BloclyActivity extends Activity {
                 BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
                 Toast.LENGTH_LONG).show();
 
+        TextView mAppTitle = (TextView) findViewById(R.id.appTitle);
+        mAppTitle.setText(R.string.app_title);
     }
 }
