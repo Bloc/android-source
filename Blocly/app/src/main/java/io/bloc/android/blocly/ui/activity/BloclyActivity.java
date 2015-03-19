532f5fa3e2181bf91bfa5341fc8cce9a9ca5c64c
Renee CS <voiceofrae@gmail.com>
2015-03-18 22:12:50 -0700

    checkpoint 36 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,29 +2,39 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
 /**
  * Created by ReneeCS on 3/17/15.
  */
 public class BloclyActivity extends Activity {
 
-    public TextView mAppTitle;
+//    public TextView mAppTitle;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
 
-        Toast.makeText(this,
-                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                Toast.LENGTH_LONG).show();
+//        Toast.makeText(this,
+//                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
+//                Toast.LENGTH_LONG).show();
 
-        TextView mAppTitle = (TextView) findViewById(R.id.appTitle);
-        mAppTitle.setText(R.string.app_title);
+        itemAdapter = new ItemAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+// #12
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
+
+//        TextView mAppTitle = (TextView) findViewById(R.id.appTitle);
+//        mAppTitle.setText(R.string.app_title);
     }
 }
