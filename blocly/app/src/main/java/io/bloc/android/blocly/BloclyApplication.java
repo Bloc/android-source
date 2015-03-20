3cfb24a05eba6bfdf619e5dd7c719008c54142ea
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-03-20 00:06:45 -0700

    Checkpoint 38 assignment completed

--- blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
+++ blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -2,6 +2,12 @@ package io.bloc.android.blocly;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 import io.bloc.android.blocly.api.DataSource;
 
 /**
@@ -29,6 +35,24 @@ import io.bloc.android.blocly.api.DataSource;
             super.onCreate();
             sharedInstance = this;
             dataSource = new DataSource();
+            // #1
+            DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                    .cacheOnDisk(true)
+                    .cacheInMemory(true)
+                    .build();
+
+// #2
+            ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(this)
+                    .tasksProcessingOrder(QueueProcessingType.LIFO)
+                    .denyCacheImageMultipleSizesInMemory()
+                    .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                    .memoryCacheSize(2 * 1024 * 1024)
+                    .diskCacheSize(50 * 1024 * 1024)
+                    .diskCacheFileCount(100)
+                    .defaultDisplayImageOptions(defaultOptions)
+                    .build();
+
+            ImageLoader.getInstance().init(configuration);
         }
 
         public DataSource getDataSource() {
--- blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -1,10 +1,17 @@
 package io.bloc.android.blocly.ui.adapter;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
@@ -16,7 +23,9 @@ import io.bloc.android.blocly.api.model.RssItem;
  * Created by tonyk_000 on 3/18/2015.
  */
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
-
+    // #11
+    private static String TAG = ItemAdapter.class.getSimpleName();
+    private static Throwable tr = new Throwable("this is a throwable");
     // #6
     @Override
     public ItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
@@ -38,11 +47,15 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
     }
 
     // #9
-    class ItemAdapterViewHolder extends RecyclerView.ViewHolder {
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener {
 
         TextView title;
         TextView feed;
         TextView content;
+        View headerWrapper;
+        ImageView headerImage;
+        // #6
+        String imageURL;
 
         public ItemAdapterViewHolder(View itemView) {
             super(itemView);
@@ -50,12 +63,52 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
             title = (TextView) itemView.findViewById(R.id.tv_rss_item_title);
             feed = (TextView) itemView.findViewById(R.id.tv_rss_item_feed_title);
             content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
+            // #7
+            headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
+            headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
         }
 
         void update(RssFeed rssFeed, RssItem rssItem) {
             feed.setText(rssFeed.getTitle());
             title.setText(rssItem.getTitle());
             content.setText(rssItem.getDescription());
+
+            imageURL = rssItem.getImageUrl();
+            if (imageURL != null) {
+// #8
+                headerWrapper.setVisibility(View.VISIBLE);
+                headerImage.setVisibility(View.INVISIBLE);
+                ImageLoader.getInstance().loadImage(imageURL, this);
+            } else {
+// #9
+                headerWrapper.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onLoadingStarted(String imageUri, View view) {
+            Log.i(TAG,"onLoadingStarted");
+        }
+
+        @Override
+        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+            Log.e(TAG, "onLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+        }
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            Log.i(TAG,"onLoadingComplete",tr);
+// #10
+            if (imageUri.equals(imageURL)) {
+                headerImage.setImageBitmap(loadedImage);
+                headerImage.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onLoadingCancelled(String imageUri, View view) {
+            // Attempt a retry
+            ImageLoader.getInstance().loadImage(imageUri, this);
         }
     }
 }
