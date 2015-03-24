af31e981da55caf95cf396c3c9a20bcafc1a4bb6
Renee CS <voiceofrae@gmail.com>
2015-03-23 20:44:35 -0700

    checkpoint 38 assignment completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -2,6 +2,12 @@ package io.bloc.android.blocly;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 import io.bloc.android.blocly.api.DataSource;
 
 /**
@@ -13,7 +19,6 @@ public class BloclyApplication extends Application {
         return sharedInstance;
     }
 
-    // #2
     public static DataSource getSharedDataSource() {
         return BloclyApplication.getSharedInstance().getDataSource();
     }
@@ -21,16 +26,35 @@ public class BloclyApplication extends Application {
     private static BloclyApplication sharedInstance;
     private DataSource dataSource;
 
-    // #3
     @Override
     public void onCreate() {
         super.onCreate();
         sharedInstance = this;
         dataSource = new DataSource();
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .build();
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(this)
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+
+        ImageLoader.getInstance().init(configuration);
+
     }
 
     public DataSource getDataSource() {
         return dataSource;
     }
+
+
 
 }
--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -1,10 +1,17 @@
 package io.bloc.android.blocly.ui.adapter;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
@@ -17,44 +24,85 @@ import io.bloc.android.blocly.api.model.RssItem;
  */
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
 
+    private static String TAG = ItemAdapter.class.getSimpleName();
+
     @Override
     public ItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
         View inflate = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rss_item, viewGroup, false);
         return new ItemAdapterViewHolder(inflate);
     }
 
-    // #7
     @Override
     public void onBindViewHolder(ItemAdapterViewHolder itemAdapterViewHolder, int index) {
         DataSource sharedDataSource = BloclyApplication.getSharedDataSource();
         itemAdapterViewHolder.update(sharedDataSource.getFeeds().get(0), sharedDataSource.getItems().get(index));
     }
 
-    // #8
     @Override
     public int getItemCount() {
         return BloclyApplication.getSharedDataSource().getItems().size();
     }
 
-    // #9
-    class ItemAdapterViewHolder extends RecyclerView.ViewHolder {
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener {
 
         TextView title;
         TextView feed;
         TextView content;
+        View headerWrapper;
+        ImageView headerImage;
+
+        String imageURL;
 
         public ItemAdapterViewHolder(View itemView) {
             super(itemView);
-// #10
             title = (TextView) itemView.findViewById(R.id.tv_rss_item_title);
             feed = (TextView) itemView.findViewById(R.id.tv_rss_item_feed_title);
             content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
+
+            headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
+            headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
+
         }
 
         void update(RssFeed rssFeed, RssItem rssItem) {
             feed.setText(rssFeed.getTitle());
             title.setText(rssItem.getTitle());
             content.setText(rssItem.getDescription());
+            imageURL = rssItem.getImageUrl();
+            if (imageURL != null) {
+// #8
+                headerWrapper.setVisibility(View.VISIBLE);
+                headerImage.setVisibility(View.INVISIBLE);
+                ImageLoader.getInstance().loadImage(imageURL, this);
+            } else {
+// #9
+                headerWrapper.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onLoadingStarted(String imageUri, View view) {}
+
+
+        @Override
+        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+            Log.e(TAG, "onLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+            Log.d(TAG, "debug here:" + failReason.toString() + " for URL: " + imageUri);
+        }
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+// #10
+            if (imageUri.equals(imageURL)) {
+                headerImage.setImageBitmap(loadedImage);
+                headerImage.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onLoadingCancelled(String imageUri, View view) {
+            // Attempt a retry
+            ImageLoader.getInstance().loadImage(imageUri, this);
         }
     }
 
