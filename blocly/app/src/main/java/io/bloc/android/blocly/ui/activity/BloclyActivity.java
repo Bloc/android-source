2ae3a4cab1d6249d2fb2a84a0bf554d178d2c050
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-03-31 20:09:46 -0700

    checkpoint 43 completed

--- blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,10 +1,15 @@
 package io.bloc.android.blocly.ui.activity;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
@@ -12,12 +17,17 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by tonyk_000 on 3/9/2015.
  */
-public class BloclyActivity extends Activity{
+public class BloclyActivity extends ActionBarActivity {
     private ItemAdapter itemAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
 @Override
     protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_blocly);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
+        setSupportActionBar(toolbar);
 
     itemAdapter = new ItemAdapter();
 
@@ -26,8 +36,33 @@ public class BloclyActivity extends Activity{
     recyclerView.setLayoutManager(new LinearLayoutManager(this));
     recyclerView.setItemAnimator(new DefaultItemAnimator());
     recyclerView.setAdapter(itemAdapter);
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    drawerLayout = (DrawerLayout) findViewById(R.id.dl_activity_blocly);
+
+    drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0,0);
+    drawerLayout.setDrawerListener(drawerToggle);
 }
 
-
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
 
