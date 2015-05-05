456724fdafe2ab731c087dd40f86934910f42af6
Tee <bunth667@gmail.com>
2015-05-05 19:22:08 -0400

    uploaded work

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -12,27 +12,29 @@ public class WorstPractices extends Object {
 	/************************************************/
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
+		int MAGIC_NUMBER = WorstPractices.animals(false);
+			MAGIC_NUMBER *= 5;
 	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
+		while(MAGIC_NUMBER > 0)
 	{
 	MAGIC_NUMBER--;
-	}
-	}
-	}
+			}
+		}
+	}	
 
 // animals
 // this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
 // yeswellheresthething: a seed which helps generate the magic number
 // returns: a magical number
-private static int animals(boolean yeswellheresthething)
+
+
+	private static int animals(boolean yeswellheresthething)
 {
 		/*
 			Start off with one of these
 		*/
 	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
+		float sparklesfairy = .5f;
 	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
 	} return (int) sparklesfairy * aInt;
 }
