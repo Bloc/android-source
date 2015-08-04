c180cbf0acb77cdda44c438ac6e9479d242c7c23
i-zor066 <igor.zor@gmail.com>
2015-08-04 12:23:07 +0200

    Checkpoint 12 completed

--- foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
+++ foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
@@ -8,13 +8,13 @@ public class SyntaxDisaster extends Object {
 	 *	Fix the code found below this block
 	/************************************************/
 
-	public static void main(String [] args) {;}
-		if (true); {
-			int x = 5
-		}
-		int syntaxIsFun; 5;
-		/* The following line prints stuff out, I promise
-		System.out.println("If you can see this message, you've won!);
+	public static void main(String [] args) {
+		if (true) {
+			int x = 5;
+		};
+		int syntaxIsFun = 5;
+		// The following line prints stuff out, I promise
+		System.out.println("If you can see this message, you've won!");
 	}
 
 	/************************************************
