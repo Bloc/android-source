65a42b87f5e918aaf1d389e4c18b9fe21e664e4b
Avan <mujibsardar@gmail.com>
2015-08-18 13:31:27 -0700

    Checkpoint 26 completed

--- foundation/26-generics/src/com/bloc/generics/Main.java
+++ foundation/26-generics/src/com/bloc/generics/Main.java
@@ -12,7 +12,17 @@ public class Main extends Object {
  		 *	ASSIGNMENT:
  		 *	Place several Toy objects into toyBox
 		/************************************************/
-
+		
+		Toy<ActionFigure> toy = new Toy<ActionFigure>(new ActionFigure());
+		Toy<Book> toy2 = new Toy<Book>(new Book());
+		Toy<Book> toy3 = new Toy<Book>(new Book());
+		Toy<Spoon> toy4 = new Toy<Spoon>(new Spoon());
+		
+		toyBox.addToy(toy);
+		toyBox.addToy(toy2);
+		toyBox.addToy(toy3);
+		toyBox.addToy(toy4);
+		
 		assert toyBox.getToyCount() > 0 : "Let's get some toys in that box!";
 		System.out.println("Inside your toybox you've got:");
 		for (int i = 0; i < toyBox.getToyCount(); i++) {
