9e70083b1b90df459f1029413a4b959e34e3f026
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-10-06 18:15:08 -0500

    heres this junk

--- foundation/20-inheritance/src/com/bloc/inherit/Chihuahua.java
+++ foundation/20-inheritance/src/com/bloc/inherit/Chihuahua.java
@@ -7,3 +7,14 @@ package com.bloc.inherit;
  *	Chiuahuas must be fed 5 times in order to grow
  *	larger.
 /************************************************/
+
+class Chihuahua extends Dog{
+  void feed() {
+		mWeight += WEIGHT_GAINED_FROM_FEEDING;
+		// Pre-increment feed counter
+		if (++mFeedCounter == 5) {
+			changeSize(true);
+			mFeedCounter = 0;
+		}
+	}
+}
--- foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
+++ foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
@@ -7,3 +7,18 @@ package com.bloc.inherit;
  *	Golden Retrievers shrink to a smaller size after
  *	playing only 3 times.
 /************************************************/
+
+class GoldenRetriever extends Dog{
+    @Override
+    void play() {
+	setWeight(getWeight() - WEIGHT_LOST_FROM_FEEDING);
+	if (getWeight() < MINIMUM_WEIGHT) {
+		setWeight(MINIMUM_WEIGHT);
+	    }
+	// Pre-increment play counter
+	if (++mPlayCounter == 3) {
+		changeSize(false);
+		mPlayCounter = 0;
+	}
+    }
+}
