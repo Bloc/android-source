8eab2d89240aa2622262da98f4fe409999bd2164
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-10-11 17:48:12 -0500

    25 polymorphism addition

--- foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
+++ foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
@@ -35,11 +35,11 @@ public class Main extends Object {
 	 	 *	Replace the operations above by employing polymorphism
 		/************************************************/
 
-		assert dog.isFed() && dog.isWashed() && dog.isExercised() : "Your dog needs a little more attention";
-		assert cat.isFed() && cat.isWashed() && cat.isExercised() : "Your cat needs a little more attention";
-		assert bird.isFed() && bird.isWashed() && bird.isExercised() : "Your bird needs a little more attention";
-		assert snake.isFed() && snake.isWashed() && snake.isExercised() : "Your snake needs a little more attention";
-		assert tarantula.isFed() && tarantula.isWashed() && tarantula.isExercised() : "Your tarantula needs a little more attention";
+		for(int i = 0; i < pets.length; i++){
+			assert pets[i].isFed() && pets[i].isWashed() && pets[i].isExercised() : "Your " + pets[i] + " needs a little more attention";
+		}
+
+		
 		
 		System.out.println("/************************/");
 		System.out.println("/*                      */");
