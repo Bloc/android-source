5b8e5efd8e1cfa56766ee6f7557d4bc365faac64
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-10-14 14:35:50 -0500

    Progress on 31 threads

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,7 +1,24 @@
 package com.bloc.threads;
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
 
 public class ImageGetter extends Thread {
 
+	URL mURL;
+
+	boolean openWhenCompleted;
+
+	public ImageGetter(String url, boolean openWhenCompleted){
+		try{
+			mURL = new URL(url);
+		}
+		catch(Exception e){
+			System.out.println("Invalid URL");
+		}
+
+	}
 	/*
 	 * ImageGetter
 	 *
@@ -21,10 +38,26 @@ public class ImageGetter extends Thread {
 
 	@Override
 	public void run() {
-		/************************************************
- 		 *	ASSIGNMENT:
- 		 *	Perform the work found in `Main.java` in this
- 		 *	Thread instead.
-		/************************************************/
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			
+			BufferedImage bufferedImage = ImageIO.read(mURL);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
--- foundation/31-threads/src/com/bloc/threads/Main.java
+++ foundation/31-threads/src/com/bloc/threads/Main.java
@@ -1,9 +1,7 @@
 package com.bloc.threads;
-
-import java.net.URL;
 import java.io.*;
 import javax.imageio.*;
-import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public class Main extends Object {
 
@@ -14,28 +12,7 @@ public class Main extends Object {
 		 *	Perform the following block of code on a separate
 		 *	Thread, use the ImageGetter class.
 		/************************************************/
-
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		new ImageGetter("http://imgur.com/gallery/gYoddrU", true).start();
 
 		/************************************************
 		 *	ASSIGNMENT:
