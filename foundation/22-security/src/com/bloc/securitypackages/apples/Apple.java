d4c07e92d62967cd00c0325d1e8f248b7874ac66
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-10-15 13:56:38 -0500

    Assignment 22 completed

--- foundation/22-security/src/com/bloc/securitypackages/apples/Apple.java
+++ foundation/22-security/src/com/bloc/securitypackages/apples/Apple.java
@@ -6,6 +6,10 @@ import com.bloc.securitypackages.Fruit;
 
 public abstract class Apple extends Fruit {
 
+	public Apple(String name, int calories, Color color, double weight) {
+        super(name, calories, color, weight);
+    }
+
    	abstract void bite();
 
 }
