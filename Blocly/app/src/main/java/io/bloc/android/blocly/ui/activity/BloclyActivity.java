eaa25b41f21726f1a22c0c28d84abcaca3897675
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-10-19 16:03:17 -0500

    Assignment 36 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
-import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
@@ -22,11 +20,6 @@ public class BloclyActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-
-        Toast.makeText(this,
-                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                Toast.LENGTH_LONG).show();
-
 
         itemAdapter = new ItemAdapter();
 
