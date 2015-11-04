840f5fdb68020e958ea9176419a3147681ed4359
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-11-04 17:15:47 -0600

    Assignment 39 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/NavigationDrawerAdapter.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/NavigationDrawerAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -69,6 +70,7 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
         TextView title;
         View bottomPadding;
         View divider;
+        ImageView shapeView;
 
         int position;
         RssFeed rssFeed;
