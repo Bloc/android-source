217a4d8f2240780dd809f75ac3ae8e76194166be
Austin Oestreich <amichaeloestreich@yahoo.com>
2015-11-05 23:12:43 -0600

    assignment 46 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -150,7 +150,18 @@ public class BloclyActivity extends AppCompatActivity implements NavigationDrawe
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        if(item.getItemId() == R.id.action_share){
+            Toast.makeText(this, "I love to share", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.action_search){
+            Toast.makeText(this, "Searching is fun", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.action_refresh) {
+            Toast.makeText(this, "Refresh it up", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.action_mark_as_read) {
+            Toast.makeText(this, "Mark em all", Toast.LENGTH_SHORT).show();
+        }
         return super.onOptionsItemSelected(item);
     }
 
