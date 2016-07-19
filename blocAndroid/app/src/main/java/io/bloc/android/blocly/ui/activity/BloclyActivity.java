dc6a29729a02c6ce1eed09932676c4c257f9fd8a
Aadi <aadikapur1@gmail.com>
2016-07-19 15:31:50 -0400

    First Project

---
+++ blocAndroid/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,18 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by aadik_000 on 7/19/2016.
+ */
+public class BloclyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
