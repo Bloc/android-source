e169e5ec2dbfe92ef145d81d4accf0f0997e8201
Aadi <aadikapur1@gmail.com>
2016-07-26 14:42:17 -0400

    Lists Checkpoint Completed

--- blocAndroid/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ blocAndroid/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,25 +2,28 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
 /**
  * Created by aadik_000 on 7/19/2016.
  */
 public class BloclyActivity extends Activity {
 
+    private ItemAdapter itemAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-        Toast.makeText(this,
-                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                Toast.LENGTH_LONG).show();
-        TextView modelName = (TextView)findViewById(R.id.hello_world);
-        modelName.setText(BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle());
+        itemAdapter = new ItemAdapter();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
     }
 }
